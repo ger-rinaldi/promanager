28accb1e27b617a1fe76ae8045b9bd7149b48e79
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2023-08-24 02:56:49 -0300

    feat: agregar función para la creación de la db

--- app/db.py
+++ app/db.py
@@ -8,3 +8,29 @@ import mysql.connector
 from mysql.connector import Error as dbError
 
 from config import DB_CONFIG, DB_NAME, TEST_DB  # pylint: disable=no-name-in-module
+
+
+def crear_base_de_datos(test: bool):
+    """Crear la base de datos al:
+        - inicializar la aplicación
+        - en caso de que la base de datos no exista previamente
+        - en caso de que la schema haya sufrido una modificación
+
+    Args:
+        test (bool): indica si la aplicacion esta utilizando una base de datos
+        de testing, en tal caso crea una acorde.
+    """
+    cnx = mysql.connector.connect(**DB_CONFIG)
+    cursor = cnx.cursor()
+
+    try:
+        if test:
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {TEST_DB}")
+        else:
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
+    except dbError as exception:
+        # TODO: Loggear este output a algun lugar
+        print(f"There was an error while creating the database:\n {exception}")
+    finally:
+        cursor.close()
+        cnx.close()
