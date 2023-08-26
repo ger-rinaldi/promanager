9410398b175ec1ce897285702e2872bf4faae03f
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2023-08-26 20:35:24 -0300

    feat: agregar class de app, entorno jinja y endpoint map

--- app/application.py
+++ app/application.py
@@ -8,3 +8,12 @@ from werkzeug.routing import Map, Rule
 from werkzeug.urls import url_parse
 from werkzeug.utils import redirect
 from werkzeug.wrappers import Request, Response
+
+
+class Promanager:
+    def __init__(self) -> None:
+        template_path = os.path.join(os.path.dirname(__file__), "templates")
+        self.jinja_env = Environment(
+            loader=FileSystemLoader(template_path), autoescape=True
+        )
+        self.url_map = Map([])
