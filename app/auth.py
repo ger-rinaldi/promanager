c6f5161bf2065727179a29bfd33d13fe11563e9e
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2023-09-03 00:59:37 -0300

    refactor: agrupar metodos de validacion de usuario en una clase

--- app/auth.py
+++ app/auth.py
@@ -1,111 +1,101 @@
 import re
 
-password_required_length = 12
 
+class validate_user:
+    password_required_length = 12
+    character_complexity = 3
 
-class Errors:
-    pass_too_short = f"La contraseña debe tener un mínimo de\
-                    {password_required_length} caractéres"
+    def password_complexity_validator(password: str = None) -> bool:
+        if password is None:
+            return
 
-    pass_too_simple = "La contraseña debe tener al menos 3 letras minúscula,\
-                    3 mayúscula, 3 números y 3 caracteres especiales."
+        def _correct_match_times(
+            password: str,
+            *patterns: re,
+            times: int = validate_user.character_complexity,
+        ) -> list[bool]:
+            match_amount_per_pattern = []
 
-    invalid_email = "La dirección de email ingresada no es válida"
+            for pattern in patterns:
+                found_matches = re.findall(pattern, password)
+                total_matches = len(found_matches)
 
-    email_alreay_registered = "La dirección de email ingresada ya está sido registrada."
+                if total_matches >= times:
+                    match_amount_per_pattern.append(True)
+                else:
+                    match_amount_per_pattern.append(False)
 
+            return match_amount_per_pattern
 
-def password_complexity_validator(password: str = None) -> bool:
-    if password is None:
-        return
+        minuscula = re.compile(r"[a-z]")
+        mayuscula = re.compile(r"[A-Z]")
+        numeros = re.compile(r"[0-9]")
+        especiales = re.compile(r"[!-\.:-@\[-_]")
 
-    def _correct_match_times(
-        password: str, *patterns: re, times: int = 3
-    ) -> list[bool]:
-        match_amount_per_pattern = []
+        match_result = _correct_match_times(
+            password, minuscula, mayuscula, numeros, especiales
+        )
 
-        for pattern in patterns:
-            found_matches = re.findall(pattern, password)
-            total_matches = len(found_matches)
+        return all(match_result)
 
-            if total_matches >= times:
-                match_amount_per_pattern.append(True)
-            else:
-                match_amount_per_pattern.append(False)
+    def password_length_validator(password: str = None) -> bool:
+        if password is None:
+            return
 
-        return match_amount_per_pattern
+        required_length: int = validate_user.password_required_length
 
-    minuscula = re.compile(r"[a-z]")
-    mayuscula = re.compile(r"[A-Z]")
-    numeros = re.compile(r"[0-9]")
-    especiales = re.compile(r"[!-\.:-@\[-_]")
+        if len(password) < required_length:
+            return False
+        else:
+            return True
 
-    match_result = _correct_match_times(
-        password, minuscula, mayuscula, numeros, especiales
-    )
+    def email_address_validator(mail_address: str = None) -> bool:
+        if mail_address is None:
+            return
 
-    return all(match_result)
+        regex = re.compile(
+            r"""
+            ^                       # el string debe comenzar con
+            [a-zA-Z_0-9\\"“”]+      # 1:N caracteres que se hallen dentro de este rango
+            (                       # luego de eso, el mail PUEDE contener el siguiente grupo:
+                (\.|-|\+)               # un simbolo de 'punto', 'guion' o 'más'  previo a
+                [a-zA-Z_0-9\\"“”\s]+    # 1:N caracteres de esta lista
+            )*                      # el grupo previamente descripto puede estar entre 0 y N veces
+            @                       # el mail debe contener una arroba
+            (                       # el mail DEBE contener el siguiente grupo:
+                \w+                 # 1:N caracteres palabra, seguidos de
+                ((\.|\-)\w+)+       # 1:N grupos empezando por '.' o '-' terminen con 1:N caracteres
+                |                                # O
+                (\[(\d{3}\.){3}\d{1,3}\]){1}     # una IP como [123.123.123.123]
+            )                       # el grupo despues de la arroba es obligatorio
+            $                       # y debe encontrarse al final del string
+            """,
+            re.VERBOSE,
+        )
+        is_valid_mail = regex.fullmatch(mail_address)
 
+        if is_valid_mail is None:
+            return False
+        else:
+            return True
 
-def password_length_validator(password: str = None) -> bool:
-    if password is None:
-        return
+    def check_email_not_registered(email: str = None) -> bool:
+        if email is None:
+            return
 
-    required_length: int = password_required_length
+        from db import get_connection
 
-    if len(password) < required_length:
-        return False
-    else:
-        return True
+        cnx = get_connection()
+        cursor = cnx.cursor()
 
+        sql = "SELECT 1 FROM usuario WHERE mail = %s;"
 
-def email_address_validator(mail_address: str = None) -> bool:
-    if mail_address is None:
-        return
+        cursor.execute(sql, (email,))
 
-    regex = re.compile(
-        r"""
-        ^                       # el string debe comenzar con
-        [a-zA-Z_0-9\\"“”]+      # 1:N caracteres que se hallen dentro de este rango
-        (                       # luego de eso, el mail PUEDE contener el siguiente grupo:
-            (\.|-|\+)               # un simbolo de 'punto', 'guion' o 'más'  previo a
-            [a-zA-Z_0-9\\"“”\s]+    # 1:N caracteres de esta lista
-        )*                      # el grupo previamente descripto puede estar entre 0 y N veces
-        @                       # el mail debe contener una arroba
-        (                       # el mail DEBE contener el siguiente grupo:
-            \w+                 # 1:N caracteres palabra, seguidos de
-            ((\.|\-)\w+)+       # 1:N grupos que empiecen por '.' o '-' terminen con 1:N caracteres
-            |                                # O
-            (\[(\d{3}\.){3}\d{1,3}\]){1}     # una IP como [123.123.123.123]
-        )                       # el grupo despues de la arroba es obligatorio
-        $                       # y debe encontrarse al final del string
-        """,
-        re.VERBOSE,
-    )
-    is_valid_mail = regex.fullmatch(mail_address)
+        email_exists = cursor.fetchone()
 
-    if is_valid_mail is None:
-        return False
-    else:
-        return True
+        if not email_exists:
+            return True
+        else:
+            return False
 
-
-def check_email_not_registered(email: str = None) -> bool:
-    if email is None:
-        return
-
-    from db import get_connection
-
-    cnx = get_connection()
-    cursor = cnx.cursor()
-
-    sql = "SELECT 1 FROM usuario WHERE mail = %s;"
-
-    cursor.execute(sql, (email,))
-
-    email_exists = cursor.fetchone()
-
-    if not email_exists:
-        return True
-    else:
-        return False
