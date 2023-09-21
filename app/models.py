895b26f0060f8a13ee39f9ccb2fcd66e80f4594b
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2023-09-21 17:12:22 -0300

    refactor: enforzar uso de type annotations en models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,11 @@
+from typing import Any, Sequence, Union
+
 from bcrypt import gensalt, hashpw
 from db import get_connection
+from mysql.connector.connection import MySQLConnection
+from mysql.connector.cursor import CursorBase
+from mysql.connector.pooling import PooledMySQLConnection
+from mysql.connector.types import RowType
 
 
 class usuario:
@@ -29,8 +35,8 @@ class usuario:
     ) -> None:
         "Crear nuevo registro de usuario en la base de datos"
 
-        cnx = get_connection()
-        cursor_create = cnx.cursor()
+        cnx: MySQLConnection | PooledMySQLConnection = get_connection()
+        cursor_create: CursorBase = cnx.cursor()
 
         sql = f"INSERT INTO {cls.tablename}({cls.no_id_fields})\
                 VALUES  (%s, %s, %s, %s, %s, %s)"
@@ -51,41 +57,36 @@ class usuario:
         cnx.close()
 
     @classmethod
-    def load_user(cls, id: int) -> "usuario":
+    def load_user(cls, id: int) -> Union["usuario", None]:
         sql = f"SELECT {cls.no_pass_fields} FROM {cls.tablename} WHERE id = %s"
 
-        cnx = get_connection()
-        cursor = cnx.cursor()
+        cnx: MySQLConnection | PooledMySQLConnection = get_connection()
+        cursor: CursorBase = cnx.cursor(dictionary=True)
 
-        loaded_user = usuario()
         cursor.execute(sql, (id,))
 
-        (
-            loaded_user.id,
-            loaded_user.nombre,
-            loaded_user.apellido,
-            loaded_user.mail,
-        ) = cursor.fetchone()
+        loaded_user: RowType | Sequence[Any] | None = cursor.fetchone()
 
-        return loaded_user
+        if loaded_user is not None:
+            return_user: "usuario" = usuario(**loaded_user)  # type: ignore
+
+        return return_user  # type: ignore
 
     def __init__(
         self,
-        id: int = None,
-        nombre: str = None,
-        apellido: str = None,
-        mail: str = None,
-        telef_prefix: str = None,
-        telef_num: str = None,
-        contraseña: str = None,
+        id: int,
+        nombre: str,
+        apellido: str,
+        email: str,
+        telefono_prefijo: str,
+        telefono_numero: str,
     ) -> None:
         self.id = id
         self.nombre = nombre
         self.apellido = apellido
-        self.mail = mail
-        self._contraseña = contraseña
-        self.prefijo_telefono = telef_prefix
-        self.numero_telefono = telef_num
+        self.mail = email
+        self.prefijo_telefono = telefono_prefijo
+        self.numero_telefono = telefono_numero
 
     def __tuple__(self) -> tuple:
         """Retornar atributos de usuario como tupla
@@ -106,7 +107,7 @@ class prefijos_telefonicos:
     def read_all(cls):
         """Obtener todos los paises y sus codigos sin ID"""
 
-        cnx = get_connection()
+        cnx: MySQLConnection | PooledMySQLConnection = get_connection()
         cursor_getall = cnx.cursor(dictionary=True)
         cursor_getall.execute(
             f"SELECT id, prefijo, pais FROM {prefijos_telefonicos.tablename}"
@@ -115,14 +116,19 @@ class prefijos_telefonicos:
         return cursor_getall.fetchall()
 
     @classmethod
-    def get_prefix_of_id(self, id: int):
+    def get_prefix_of_id(cls, id: int) -> Union[str, None]:
         """Obtener todos los paises y sus codigos sin ID"""
 
-        cnx = get_connection()
-        cursor_prefix = cnx.cursor()
+        cnx: MySQLConnection | PooledMySQLConnection = get_connection()
+        cursor_prefix: CursorBase = cnx.cursor()
         cursor_prefix.execute(
             f"SELECT prefijo FROM {prefijos_telefonicos.tablename} WHERE id = %s",
             (id,),
         )
 
-        return cursor_prefix.fetchone()
+        prefix: RowType | Sequence[Any] | None = cursor_prefix.fetchone()
+
+        if prefix is not None:
+            return str(prefix[0])
+
+        return None
