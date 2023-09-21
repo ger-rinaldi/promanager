b1ba9ab53e99f6bac77808eeaf26bf5d75d107e0
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2023-09-21 15:24:27 -0300

    refactor: mejorar legibilidad de schemas
    los nombres de los atributos de cada tabla han sido cambiados
    por alternativas mas legibles.

--- app/schema.sql
+++ app/schema.sql
@@ -2,13 +2,13 @@
 -- para manejar individualmente cada creacion y sus posibles errores
 -- table
 CREATE TABLE IF NOT EXISTS prefijo_telefono (
-    ID_prefijo INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     prefijo VARCHAR(8) NOT NULL,
     pais VARCHAR(60) NOT NULL
 );
 -- table
 CREATE TABLE IF NOT EXISTS proyecto(
-    ID_proyecto INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(60) NOT NULL DEFAULT 'not_named',
     descripcion TINYTEXT,
     es_publico BOOLEAN NOT NULL DEFAULT false,
@@ -19,71 +19,71 @@ CREATE TABLE IF NOT EXISTS proyecto(
 );
 -- table
 CREATE TABLE IF NOT EXISTS usuario(
-    ID_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(60) NOT NULL DEFAULT 'no_name',
     apellido VARCHAR(60) NOT NULL DEFAULT 'no_surname',
-    mail VARCHAR(320) UNIQUE NOT NULL,
-    prefijo_tel INT NOT NULL,
-    telefono_num VARCHAR(30) NOT NULL,
+    email VARCHAR(320) UNIQUE NOT NULL,
+    telefono_prefijo INT NOT NULL,
+    telefono_numero VARCHAR(30) NOT NULL,
     contraseña VARCHAR(72) NOT NULL,
-    FOREIGN KEY (prefijo_tel) REFERENCES prefijo_telefono(ID_prefijo)
+    FOREIGN KEY (telefono_prefijo) REFERENCES prefijo_telefono(id)
 
 );
 -- table
 CREATE TABLE IF NOT EXISTS roles_proyecto (
-    ID_rol_proyecto INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(60) UNIQUE NOT NULL
 );
 -- table
 CREATE TABLE IF NOT EXISTS detalle_proyecto(
-    ID_detalle_proyecto INT AUTO_INCREMENT PRIMARY KEY,
-    ID_proyecto INT NOT NULL,    /*FORANEA*/
-    ID_usuario INT NOT NULL, /*FORANEA*/
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    proyecto INT NOT NULL,    /*FORANEA*/
+    integrante INT NOT NULL, /*FORANEA*/
     rol INT NOT NULL, /*FORANEA*/
     fecha_inicio_participacion DATE NOT NULL DEFAULT (CURRENT_DATE()),
     fecha_baja DATE NOT NULL DEFAULT '1000-01-01', 
     suspendido BOOLEAN NOT NULL DEFAULT false,
-    FOREIGN KEY (ID_proyecto) REFERENCES proyecto(ID_proyecto),
-    FOREIGN KEY (ID_usuario) REFERENCES usuario(ID_usuario),
-    FOREIGN KEY (rol) REFERENCES roles_proyecto(ID_rol_proyecto),
-    CONSTRAINT integrante_proyecto UNIQUE (ID_proyecto, ID_usuario)
+    FOREIGN KEY (proyecto) REFERENCES proyecto(id),
+    FOREIGN KEY (integrante) REFERENCES usuario(id),
+    FOREIGN KEY (rol) REFERENCES roles_proyecto(id),
+    CONSTRAINT integrante_proyecto UNIQUE (proyecto, integrante)
 );
 -- table
 CREATE TABLE IF NOT EXISTS equipo(
-    ID_equipo INT AUTO_INCREMENT PRIMARY KEY,
-    ID_proyecto INT NOT NULL,    /*FORANEA*/
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    proyecto INT NOT NULL,    /*FORANEA*/
     nombre VARCHAR(60) NOT NULL DEFAULT 'not_named',
     fecha_creacion DATE NOT NULL DEFAULT (CURRENT_DATE()),
-    FOREIGN KEY (ID_proyecto) REFERENCES proyecto(ID_proyecto)
+    FOREIGN KEY (proyecto) REFERENCES proyecto(id)
 );
 -- table
 CREATE TABLE IF NOT EXISTS roles_equipo (
-    ID_rol_equipo INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(60) UNIQUE NOT NULL
 );
 -- table
 CREATE TABLE IF NOT EXISTS detalle_equipo(
-    ID_detalle_equipo INT AUTO_INCREMENT PRIMARY KEY,
-    ID_equipo INT NOT NULL,  /*FORANEA*/
-    ID_usuario INT NOT NULL, /*FORANEA*/
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    equipo INT NOT NULL,  /*FORANEA*/
+    miembro INT NOT NULL, /*FORANEA*/
     rol INT NOT NULL,
     fecha_inicio_participacion DATE NOT NULL DEFAULT (CURRENT_DATE()),
     fecha_baja DATE NOT NULL DEFAULT '1000-01-01',
     suspendido BOOLEAN NOT NULL DEFAULT false,
-    FOREIGN KEY (ID_equipo) REFERENCES equipo(ID_equipo),
-    FOREIGN KEY (ID_usuario) REFERENCES usuario(ID_usuario),
-    FOREIGN KEY (rol) REFERENCES roles_equipo(ID_rol_equipo),
-    CONSTRAINT integrante_equipo UNIQUE (ID_equipo, ID_usuario)
+    FOREIGN KEY (equipo) REFERENCES equipo(id),
+    FOREIGN KEY (miembro) REFERENCES usuario(id),
+    FOREIGN KEY (rol) REFERENCES roles_equipo(id),
+    CONSTRAINT integrante_equipo UNIQUE (equipo, miembro)
 );
 -- table
 CREATE TABLE IF NOT EXISTS estado(
-    ID_estado INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nombre_estado VARCHAR(25) UNIQUE NOT NULL
 );
 -- table
 CREATE TABLE IF NOT EXISTS hito(
-    ID_hito INT AUTO_INCREMENT PRIMARY KEY,
-    ID_proyecto INT NOT NULL,    /*FORANEA*/
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    proyecto INT NOT NULL,    /*FORANEA*/
     nombre VARCHAR(60) NOT NULL DEFAULT 'not_named',
     estado INT NOT NULL,
     descripcion TINYTEXT,
@@ -91,14 +91,14 @@ CREATE TABLE IF NOT EXISTS hito(
     fecha_creacion DATE NOT NULL DEFAULT (CURRENT_DATE()),
     fecha_limite DATE NOT NULL DEFAULT '1000-01-01',
     fecha_finalizacion DATE NOT NULL DEFAULT '1000-01-01',
-    FOREIGN KEY (ID_proyecto) REFERENCES proyecto(ID_proyecto),
-    FOREIGN KEY (estado) REFERENCES estado(ID_estado)
+    FOREIGN KEY (proyecto) REFERENCES proyecto(id),
+    FOREIGN KEY (estado) REFERENCES estado(id)
 );
 -- table
 CREATE TABLE IF NOT EXISTS ticket(
-    ID_ticket INT AUTO_INCREMENT PRIMARY KEY,
-    ID_hito INT NOT NULL,    /*FORANEA*/
-    ID_usuario INT NOT NULL, /*FORANEA*/
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    hito INT NOT NULL,    /*FORANEA*/
+    responsable INT NOT NULL, /*FORANEA*/
     nombre VARCHAR(60) NOT NULL DEFAULT 'not_named',
     estado INT NOT NULL,
     descripcion TINYTEXT,
@@ -106,22 +106,22 @@ CREATE TABLE IF NOT EXISTS ticket(
     fecha_asignacion DATE NOT NULL DEFAULT '1000-01-01',
     fecha_limite DATE NOT NULL DEFAULT '1000-01-01',
     fecha_finalizacion DATE NOT NULL DEFAULT '1000-01-01',
-    FOREIGN KEY (ID_usuario) REFERENCES usuario(ID_usuario),
-    FOREIGN KEY (ID_hito) REFERENCES hito(ID_hito),
-    FOREIGN KEY (estado) REFERENCES estado(ID_estado)
+    FOREIGN KEY (responsable) REFERENCES usuario(id),
+    FOREIGN KEY (hito) REFERENCES hito(id),
+    FOREIGN KEY (estado) REFERENCES estado(id)
 );
 -- table
 CREATE TABLE IF NOT EXISTS unidad_trabajo(
-    ID_unidad_trabajo INT AUTO_INCREMENT PRIMARY KEY,
-    ID_proyecto INT NOT NULL, /*FORANEA*/
-    ID_equipo INT NOT NULL,  /*FORANEA*/
-    ID_hito INT NOT NULL,    /*FORANEA*/
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    proyecto INT NOT NULL, /*FORANEA*/
+    equipo INT NOT NULL,  /*FORANEA*/
+    hito INT NOT NULL,    /*FORANEA*/
     fecha_creacion DATE NOT NULL DEFAULT (CURRENT_DATE()),
     fecha_asignacion DATE NOT NULL DEFAULT '1000-01-01',
     fecha_limite DATE NOT NULL DEFAULT '1000-01-01',
     fecha_finalizacion DATE NOT NULL DEFAULT '1000-01-01',
-    FOREIGN KEY (ID_proyecto) REFERENCES proyecto(ID_proyecto),
-    FOREIGN KEY (ID_equipo) REFERENCES equipo(ID_equipo),
-    FOREIGN KEY (ID_hito) REFERENCES hito(ID_hito),
-    CONSTRAINT unidad_equipo_hito UNIQUE (ID_equipo, ID_hito)
+    FOREIGN KEY (proyecto) REFERENCES proyecto(id),
+    FOREIGN KEY (equipo) REFERENCES equipo(id),
+    FOREIGN KEY (hito) REFERENCES hito(id),
+    CONSTRAINT unidad_equipo_hito UNIQUE (equipo, hito)
 );
