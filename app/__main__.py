34cc6e9fed4708607442eb72fb46f136581f3c5e
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2023-09-23 05:28:36 -0300

    refactor: mover create_app a main para probar refactorizacion

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,25 @@
-from application import create_app
+import os
+
 from config import APP_CONFIG
+from werkzeug.middleware.shared_data import SharedDataMiddleware
+from wsgi_app import render_template, wsgi
+
+
+def create_app(with_static=True):
+    new_app = wsgi()
+
+    if with_static:
+        new_app.app = SharedDataMiddleware(
+            new_app.app,
+            {"/static": os.path.join(os.path.dirname(__file__), "static")},
+        )
+
+    @new_app.add_route("/")
+    def home_page(request):
+        return render_template("home.html")
+
+    return new_app
+
 
 if __name__ == "__main__":
     app = create_app()
