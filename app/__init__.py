991ed009acaae524fa05b1ab43f1382fa0507626
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2023-09-25 22:17:09 -0300

    refactor: cumplir con configuracion de deploy
    se extrajo create_app de main, siendo esa la configuracion
    de testeo, al hayarse en init, facilita su obtencion
    en la configuracion del servidor wsgi de pythonanywhere

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+import os
+
+from werkzeug.middleware.shared_data import SharedDataMiddleware
+from wsgi_app import make_response, render_template, wsgi
+
+
+def create_app(with_static=True):
+    new_app = wsgi()
+
+    if with_static:
+        new_app.app = SharedDataMiddleware(
+            new_app.app,
+            {"/static": os.path.join(os.path.dirname(__file__), "static")},
+        )
+
+    @new_app.route(["/", "/home"])
+    def home_page(request):
+        return make_response(render_template("home.html"))
+
+    import auth
+
+    new_app.register_blueprint(auth.bp)
+
+    return new_app
--- app/__main__.py
+++ app/__main__.py
@@ -1,29 +1,5 @@
-import os
-
+from __init__ import create_app
 from config import APP_CONFIG
-from werkzeug.middleware.shared_data import SharedDataMiddleware
-from wsgi_app import make_response, render_template, wsgi
-
-
-def create_app(with_static=True):
-    new_app = wsgi()
-
-    if with_static:
-        new_app.app = SharedDataMiddleware(
-            new_app.app,
-            {"/static": os.path.join(os.path.dirname(__file__), "static")},
-        )
-
-    @new_app.route(["/", "/home"])
-    def home_page(request):
-        return make_response(render_template("home.html"))
-
-    import auth
-
-    new_app.register_blueprint(auth.bp)
-
-    return new_app
-
 
 if __name__ == "__main__":
     app = create_app()
