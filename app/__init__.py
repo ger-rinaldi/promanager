ebc386143e6126d5415ab6e9a4059bd354ca35ca
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2023-10-22 03:51:39 -0300

    feat: redirigir home_page a dashboarb si hay session

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import os
 
+from models import Usuario
 from werkzeug.middleware.shared_data import SharedDataMiddleware
+from werkzeug.utils import redirect
 from wsgi_app import make_response, render_template, request, wsgi
 
 
@@ -15,6 +17,13 @@ def create_app(with_static=True):
 
     @new_app.route(["/", "/home"])
     def home_page():
+        session_key = request.cookies.get("sessionId", False)
+        if session_key:
+            logged_user = Usuario.get_user_by_session_id(session_key)
+
+            if logged_user is not None:
+                return redirect(f"/usuario/{logged_user.username}/dashboard")
+
         return make_response(render_template("home.html"))
 
     import routes_auth
