e6d9fa14b610cba9308521dec83dde93fd12bd15
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2023-12-04 00:41:47 -0300

    feat: agregar display de mensajes recibidos de api
    Se agregaron funciones para permitir mostrar al usuario los mensajes
    recibidos de la api, como errores de validación o notificación de éxito.

--- app/static/js/project_integrants.js
+++ app/static/js/project_integrants.js
@@ -23,7 +23,53 @@ function get_resources() {
 const resources = get_resources();
 const api_url = `/api/usuario/${resources["usuario"]}/proyecto/${resources["proyecto"]}/integrante`;
 
+function getErrorDisplay(errorMsg) {
+  const errorElement = document.createElement("div");
+  errorElement.classList.add("alert-danger");
+  errorElement.classList.add("alert");
+  errorElement.textContent = errorMsg;
+  return errorElement;
+}
+
+function getSuccessDisplay(successMsg) {
+  const successElement = document.createElement("div");
+  successElement.classList.add("alert-success");
+  successElement.classList.add("alert");
+  successElement.textContent = successMsg;
+  return successElement;
+}
+
+function clearMessages() {
+  const msgDisplay = document.getElementById("message-display");
+
+  while (msgDisplay.firstChild) {
+    msgDisplay.removeChild(msgDisplay.lastChild);
+  }
+}
+
+function displayResponseMessages(responseStatus, responseMessages) {
+  clearMessages();
+  const msgDisplay = document.getElementById("message-display");
+
+  displayByStatus = (message) => {
+    if (responseStatus === 200) {
+      msgDisplay.appendChild(getSuccessDisplay(message));
+    } else {
+      msgDisplay.appendChild(getErrorDisplay(message));
+    }
+  };
+
+  if (Array.isArray(responseMessages)) {
+    for (const message of responseMessages) {
+      displayByStatus(message);
+    }
+  } else {
+    displayByStatus(responseMessages);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+  const msgDisplay = document.getElementById("message-display");
   const createButton = document.getElementById("create-button");
 
   createButton.addEventListener("click", async function () {
@@ -41,5 +87,7 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     const registerResponse = await fetch(createParticipantURL, request);
+    const responsePromise = await registerResponse.json();
+    displayResponseMessages(registerResponse.status, responsePromise.message);
   });
 });
