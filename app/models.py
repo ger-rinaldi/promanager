a247780e13d40e156814446b91ced66688522580
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-02-11 21:08:14 -0300

    feat: agregar clase equipo metodos crud
    - CRUD basicos
    - lectura de recursos asociados (miembros, tareas, proyecto padre)
    - metodos dunder de conversion (iterable, tupla y string)

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from mysql.connector.cursor import CursorBase
 from mysql.connector.pooling import PooledMySQLConnection
 from mysql.connector.types import RowType
 
-from app.db import close_conn_cursor, get_connection
+from app.db import close_conn_cursor, context_db_manager, get_connection
 
 
 class Usuario:
@@ -844,14 +844,58 @@ class Equipo:
     def __init__(
         self,
         nombre: str,
-        fecha_creacion: date,
-        proyecto: Union[int, "Proyecto"],
+        proyecto: int,
+        fecha_creacion: Optional[date] = None,
         id: Optional[int] = None,
     ) -> None:
         self.id = id
         self.nombre = nombre
         self.fecha_creacion = fecha_creacion
-        self.proyecto = proyecto
+        self.id_proyecto = proyecto
+
+    def _query_id(self):
+        with context_db_manager() as conn:
+            conn.execute("SELECT id FROM equipo WHERE nombre = %s", (self.nombre,))
+            self.id = conn.fetchone()[0]
+
+    def create(self) -> None:
+        if self.fecha_creacion is None:
+            fields = "(proyecto, nombre)"
+            value_placeholder = "(%s,%s)"
+            values = (self.id_proyecto, self.nombre)
+        else:
+            fields = "(proyecto, nombre, fecha_creacion)"
+            value_placeholder = "(%s,%s,%s)"
+            values = (self.id_proyecto, self.nombre, self.fecha_creacion)
+
+        create_query = f"INSERT INTO equipo{fields} VALUES {value_placeholder}"
+
+        with context_db_manager() as db_conn:
+            db_conn.execute(
+                create_query,
+                values,
+            )
+            db_conn.connection.commit()
+
+        self._query_id()
+
+    def update(self) -> None:
+        with context_db_manager() as conn:
+            conn.execute(
+                """UPDATE equipo 
+                SET nombre = %s, fecha_creacion = %s
+                WHERE id = %s""",
+                (self.nombre, self.fecha_creacion, self.id),
+            )
+            conn.connection.commit()
+
+    def delete(self) -> None:
+        with context_db_manager() as conn:
+            conn.execute(
+                "DELETE FROM equipo WHERE id = %s",
+                (self.id,),
+            )
+            conn.connection.commit()
 
     def user_can_modify(self, user_id):
         query = """
@@ -878,6 +922,81 @@ class Equipo:
             return True
 
         return False
+
+    def load_own_resources(self, project_class: bool) -> None:
+        self._fetch_all_tasks()
+        self._fetch_all_members()
+        self._fetch_project(project_class)
+
+    def register_new_member(self, member_id: int, role_id: int) -> None:
+        pass
+
+    def update_member(self, member_id: int, role_id: int) -> None:
+        pass
+
+    def delete_member(self, member_id: int) -> None:
+        pass
+
+    def _fetch_all_members(self) -> list[RowType]:
+        with context_db_manager(dict=True) as conn:
+            conn.execute(
+                """
+            SELECT me.id, rp.nombre as rol, u.username, u.nombre, u.apellido, u.email,
+                CONCAT(pf.prefijo, "-", u.telefono_numero) as "telefono_numero"
+            FROM
+                miembros_equipo as me
+                INNER JOIN roles_equipo as rp ON rp.id = me.rol
+                INNER JOIN usuario as u ON me.miembro = u.id
+                INNER JOIN prefijo_telefono as pf ON pf.id = u.telefono_prefijo
+            WHERE
+                me.equipo = %s
+                        """,
+                (self.id,),
+            )
+
+            self.miembros = conn.fetchall()
+
+    def _fetch_project(self, as_dict: bool) -> list[RowType]:
+        self.proyecto = Proyecto.get_by_id(self.id_proyecto)
+
+        if not as_dict:
+            self.proyecto = dict(self.proyecto)
+
+    def _fetch_all_tasks(self) -> list[RowType]:
+        with context_db_manager(dict=True) as conn:
+            conn.execute("SELECT * FROM ticket_tarea WHERE equipo = %s", (self.id,))
+
+            self.tareas = conn.fetchall()
+
+    def __tuple__(self, with_id: bool = False) -> tuple:
+        self_as_tuple: list[Any] = [
+            self.id_proyecto,
+            self.nombre,
+        ]
+
+        if self.fecha_creacion is not None:
+            self_as_tuple.append(self.fecha_creacion)
+
+        if with_id:
+            self_as_tuple.insert(0, self.id)
+
+        return tuple(self_as_tuple)
+
+    def __repr__(self) -> str:
+        return f"id: {self.id}, nombre: {self.nombre}, \
+proyecto: {self.id_proyecto}, creado: {self.fecha_creacion}"
+
+    def __iter__(self):
+        yield "id", self.id
+        yield "nombre", self.nombre
+        yield "proyecto", self.id_proyecto
+        yield "fecha_creacion", self.fecha_creacion
+
+        if hasattr(self, "miembros") and self.miembros is not None:
+            yield "miembros", self.miembros
+
+        if hasattr(self, "tareas") and self.tareas is not None:
+            yield "tareas", self.tareas
 
 
 class Roles:
