e0c024b8664355149a9b43f86cab2ef8de929100
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-02-29 18:16:54 -0300

    fix: resolver falta de autocompletion para modelos
    Previamente los modelos se importaban de manera absoluta en
    models/__init__ y se asignaban a una variable para no alterar la manera
    en que se importaban en los demas modulos, pero esto inhabilitaba el uso
    de autocompletion. Se cambio a importar en __init__ con el statement:
    from model_file import model, que sigue siendo valido y no pierde
    autocompletion.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,15 +1,6 @@
-import app.models.equipo as equipo
-import app.models.prefijo_telefonico as prefijos_telefonicos
-import app.models.proyecto as proyecto
-import app.models.roles as roles
-import app.models.ticket_tarea as ticket_tarea
-import app.models.usuario as usuario
-
-Equipo: equipo.Equipo = equipo.Equipo
-PrefijosTelefonicos: prefijos_telefonicos.prefijos_telefonicos = (
-    prefijos_telefonicos.prefijos_telefonicos
-)
-Proyecto: proyecto.Proyecto = proyecto.Proyecto
-Roles: roles.Roles = roles.Roles
-Ticket_Tarea: ticket_tarea.Ticket_Tarea = ticket_tarea.Ticket_Tarea
-Usuario: usuario.Usuario = usuario.Usuario
+from app.models.equipo import Equipo
+from app.models.prefijo_telefonico import prefijos_telefonicos
+from app.models.proyecto import Proyecto
+from app.models.roles import Roles
+from app.models.ticket_tarea import Ticket_Tarea
+from app.models.usuario import Usuario
